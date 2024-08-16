59896e972e84e39c3a087db931f40049ff1022d7
Pratham Agarwal <gargpratham71@gmail.com>
2024-08-17 00:43:45 +0530

    Footer and Navbar Fixed

--- components/Footer.js
+++ components/Footer.js
@@ -5,14 +5,13 @@ import { FaLinkedin } from "react-icons/fa";
 
 const Footer = () => {
   return (
-    <Box as="footer" py={8} bg="gray.800" color="white">
+    <Box as="footer" py={8} bg="gray.700" color="white" px={4}>
       {/* Contact and Social Links */}
       <Flex
         direction={{ base: "column", md: "row" }}
         justify={{ base: "center", md: "space-between" }}
         align={{ base: "center", md: "flex-start" }}
         flexWrap="wrap"
-        px={{ base: 4, md: 0 }}
         maxW="1200px"
         mx="auto"
       >
@@ -23,16 +22,22 @@ const Footer = () => {
           textAlign={{ base: "center", md: "left" }}
         >
           <Flex align="center" mb={4}>
-            <Icon as={FiMapPin} mr={2} />
-            <Text>Salt Lake, Kolkata- 700138</Text>
+            <Icon as={FiMapPin} mr={2} color="cyan" />
+            <Text cursor="pointer"  _hover={{ color: "green.300", transition: "color 0.3s" }}>
+              Salt Lake, Kolkata- 700138
+            </Text>
           </Flex>
           <Flex align="center" mb={4}>
-            <Icon as={FiPhone} mr={2} />
-            <Text>+91 9674177512</Text>
+            <Icon as={FiPhone} mr={2} color="red" />
+            <Text cursor="pointer" _hover={{ color: "green.300", transition: "color 0.3s" }}>
+              +91 9674177512
+            </Text>
           </Flex>
           <Flex align="center" mb={4}>
-            <Icon as={FiMail} mr={2} />
-            <Text>shaswata.ssaha@gmail.com</Text>
+            <Icon as={FiMail} mr={2} color="yellow" />
+            <Text  cursor="pointer" _hover={{ color: "green.300", transition: "color 0.3s" }}>
+              shaswata.ssaha@gmail.com
+            </Text>
           </Flex>
         </Box>
 
@@ -43,6 +48,7 @@ const Footer = () => {
             <Link
               href="https://www.linkedin.com/in/shaswata-saha-74b209251/"
               isExternal
+              _hover={{ color: "cyan.500", transition: "color 0.3s" }}
             >
               <Icon as={FaLinkedin} boxSize={6} />
             </Link>
@@ -56,12 +62,23 @@ const Footer = () => {
             <Flex>
               <Input
                 placeholder="Enter your email"
-                bg="gray.700"
+                bg="gray.600"
                 color="white"
+                size="lg"
                 mr={2}
                 _placeholder={{ color: "gray.400" }}
               />
-              <Button colorScheme="blue" size="md">
+              <Button
+                bg="gold"
+                size="lg"
+                color="black"
+                _hover={{
+                  bg: "black",
+                  color: "gold",
+                  boxShadow: "2px 2px 5px rgba(0,0,0,0.7)",
+                }}
+                transition="all 0.3s"
+              >
                 Subscribe
               </Button>
             </Flex>
@@ -76,12 +93,17 @@ const Footer = () => {
           top="-12px"
           left="50%"
           transform="translateX(-50%)"
-          bg="gray.800"
+          bg="gray.700"
           px={4}
         >
-          <Text fontSize="sm" color="gray.500">
+          <Text fontSize="sm" color="gray.400">
             Developed by{" "}
-            <Link href="https://acns.vercel.app" isExternal color="white">
+            <Link
+              href="https://acns.vercel.app"
+              isExternal
+              color="cyan.500"
+              _hover={{ color: "white", transition: "color 0.3s" }}
+            >
               ACNS
             </Link>
           </Text>
@@ -89,8 +111,8 @@ const Footer = () => {
       </Flex>
 
       {/* Copyright */}
-      <Flex justify="center" mt={4}>
-        <Text>&copy; 2024 VitalityAI</Text>
+      <Flex justify="center" mt={4} textShadow="2px 2px 5px rgba(0,0,0,0.5)">
+        <Text fontSize="lg">&copy; 2024 VitalityAI</Text>
       </Flex>
     </Box>
   );
