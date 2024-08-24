611716d938d9093f53ff7bd0f76006a24010c806
subhro1530 <shaswata.ssaha@gmail.com>
2024-08-25 01:26:13 +0530

    form done

---
+++ pages/form.js
@@ -0,0 +1,19 @@
+import Head from "next/head";
+import dynamic from "next/dynamic";
+import Footer from "@/components/Footer";
+const Navbar = dynamic(() => import("@/components/Navbar"), {
+  ssr: false,
+});
+const Form = dynamic(() => import("@/components/Form"), {
+  ssr: false,
+});
+
+export default function Form() {
+  return (
+    <>
+      <Navbar />
+      <Form />
+      <Footer />
+    </>
+  );
+}
